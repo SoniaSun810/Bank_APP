934d20f77b5478211577f75d4e8d61f9fdb15b02
Fengnan <soniasun0925@gmail.com>
2023-05-07 15:35:55 -0700

    refactor

--- backend/database.js
+++ backend/database.js
@@ -1,20 +1,31 @@
 import mysql from 'mysql2';
-
-import * as dotenv from 'dotenv';
+import dotenv from 'dotenv';
 dotenv.config()
 
-const pool = mysql.createPool({
+export const pool = mysql.createPool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
+    database: process.env.MYSQL_DATABASE,
 }).promise();
+
+
+const rows = await pool.query(`
+    SELECT *
+    FROM accounts
+`);
+console.log(rows);
 
 // get all accounts
 export async function getAccounts() {
     const [rows] = await pool.query("SELECT * FROM accounts");
     return rows;
 }
+
+/* BAD CODE
+query: SELECT * FROM accounts WHERE username = 'admin' OR 1=1; -- ' AND password = 'password'
+
+*/
 
 // get a user by the username
 export async function getAccount(username) {
